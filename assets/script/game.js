79b5a6d3a94c12136edc842b48ec321122bd21d7
Wuyue <314493146@qq.com>
2020-05-17 00:31:25 +0800

    fix 音乐播放无法加载

--- assets/script/game.js
+++ assets/script/game.js
@@ -2,6 +2,10 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+        m_audioSource: {
+            default: null,
+            type: cc.AudioClip
+        }
     },
 
     // LIFE-CYCLE CALLBACKS:
@@ -9,6 +13,7 @@ cc.Class({
     // onLoad () {},
 
     start () {
+        cc.audioEngine.play(this.m_audioSource, false, 1);
     },
 
     // update (dt) {},
