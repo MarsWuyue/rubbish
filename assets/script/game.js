1d0babaf2ba241493291c6bde0e22f36a8453197
Wuyue <314493146@qq.com>
2020-05-18 19:06:22 +0800

    update theme

--- assets/script/game.js
+++ assets/script/game.js
@@ -2,19 +2,34 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        m_audioSource: {
-            default: null,
-            type: cc.AudioClip
-        }
+        m_playButton: cc.Button,
+
     },
 
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
+    onLoad () {
+        cc.loader.loadRes('lastDance', cc.AudioClip, function (err, clip) {
+            cc.audioEngine.playMusic(clip, true);
+        });
+    },
 
     start () {
-        cc.audioEngine.play(this.m_audioSource, false, 1);
     },
+
+    playMusic () {
+        if (cc.audioEngine.isMusicPlaying()) {
+            cc.audioEngine.pauseMusic();
+            this.m_playButton.string = '继续播放';
+        } else {
+            cc.audioEngine.resumeMusic();
+            this.m_playButton.string = '暂停播放';
+        }
+    },
+
+    onDestroy: function () {
+        cc.audioEngine.stop(this.current);
+    }
 
     // update (dt) {},
 });
